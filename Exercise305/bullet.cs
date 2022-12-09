b661b3ec8959b6799fbc547424bd7c12acbbaa34
Jeuneau <boekhoudtjeuneau@gmail.com>
2022-12-09 13:17:20 +0100

    .

--- Exercise305/bullet.cs
+++ Exercise305/bullet.cs
@@ -4,9 +4,9 @@ using Raylib_cs;
 namespace Movement;
 
 class Bullet {
-    Random rd = new Random();
+    /*Random rd = new Random();
 
       int rand_num = rd.Next(100,200);
 
-      Console.WriteLine(rand_num);
+      Console.WriteLine(rand_num);*/
 }
---
+++ Exercise305/score.cs
@@ -0,0 +1,21 @@
+public class Score {
+
+private int punten;
+private int puntenTarg;
+
+
+
+punten = 0;
+puntenTarg= 0;
+}
+
+void Update {
+    if KeyboardKey {
+        puntenTarg++;
+    }
+    drawText;
+}
+
+
+
+
