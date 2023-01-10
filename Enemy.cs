6f64c1959854419d234b2201fd3eaaca758287dc
Jeuneau <boekhoudtjeuneau@gmail.com>
2023-01-10 13:23:54 +0100

    Enemy gefixt + enemies 2 en 3 toegevoegd

--- Enemy.cs
+++ Enemy.cs
@@ -7,9 +7,13 @@ namespace Movement;
 
 class Enemy : MoverNode
 {
+
+    	//Vector2 Velocity;
+		//Vector2 Acceleration;	
+		//private float topspeed= 1000;
     public Enemy() : base("resources/Alien.png")
     {
-	    Position = new Vector2(this.Position.X, this.Position.Y);
+	  
     }
 
      public override void Update(float deltaTime) {
