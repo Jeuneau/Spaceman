9dad38bdfce71782de34578ecb9b7ce512f09d83
Jeuneau <boekhoudtjeuneau@gmail.com>
2023-01-12 12:22:21 +0100

    klassediagram verbeterd en class circle begonnen

---
+++ Circle.cs
@@ -0,0 +1,17 @@
+using System; 
+using System.Numerics; 
+using Raylib_cs;
+
+namespace Movement
+{
+
+class Circle : Node {
+    private float radius = 5;
+
+    public Circle() : base() {
+        Position = new Vector2(Settings.ScreenSize.X / 2, Settings.ScreenSize.Y / 2);
+    }
+   
+}
+
+}
